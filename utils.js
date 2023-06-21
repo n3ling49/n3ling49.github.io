a810611af5d999bd590096071bbdd976e51831c8
n3ling49 <noahdreiling@gmail.com>
2023-06-21 09:39:15 +0200

    script auslagern

---
+++ utils.js
@@ -0,0 +1,6 @@
+const m = "kontak";
+const n = "t" + String.fromCharCode(64) + "tenno";
+const o = "n.io";
+const element = document.getElementById("contact");
+element.href = "mai" + "lto" + String.fromCharCode(58) + m + n + o;
+element.innerHTML = m + n + o;
