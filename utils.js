3128fff280faa4e8e161fd2b56fa9b923087679c
n3ling49 <noahdreiling@gmail.com>
2024-11-02 12:14:50 +0100

    update outdated information

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
-const m = "kontak";
-const n = "t" + String.fromCharCode(64) + "tenno";
-const o = "n.io";
+const m = "noahdreilin";
+const n = "g" + String.fromCharCode(64) + "gmai";
+const o = "l.com";
 const element = document.getElementById("contact");
 element.href = "mai" + "lto" + String.fromCharCode(58) + m + n + o;
 element.innerHTML = m + n + o;
